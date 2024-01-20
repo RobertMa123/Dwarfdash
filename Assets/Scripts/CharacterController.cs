afb266a636dfc529024eaff1139557ae811b88cd
wolf123cub <porterorvetti@gmail.com>
2024-01-19 23:19:53 -0500

    Update CharacterController.cs

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -8,25 +8,38 @@ public class CharacterController : MonoBehaviour
     public float speed;
     public float multiplier;
 
+    public float delayMultiplier;
+
     public Transform player;
+
+    private Rigidbody2D playerRigid;
 
 
     void Start()
     {
+        playerRigid = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
         LookAtMouse();
+
+        Vector2 wantedDirection = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        Vector2 currentDirection = Vector2.Lerp(playerRigid.velocity, wantedDirection, Time.deltaTime);
+
+        playerRigid.velocity = currentDirection * speed;
+
     }
     
     void FixedUpdate()
     {
-        MovePlayer();
+        //MovePlayer();
     }
 
     private void LookAtMouse()
     {
         var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+
+        
         transform.rotation = Quaternion.AngleAxis((Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg), Vector3.forward);
     }
 
