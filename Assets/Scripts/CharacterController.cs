ad755a3c8b001f1c7942568526168de5e87115fd
RobertMa123 <rmatzkin@clarku.edu>
2024-01-20 09:44:47 -0500

    Added speed increase, easy/hard mode bool

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,10 +7,15 @@ public class CharacterController : MonoBehaviour
     [Header("Speed Options")]
     public float speed;
     public float multiplier;
+    public float speedUpMultiplier;
+    public float topSpeed;
 
     public float baseDelayMultiplier;
 
     public Transform player;
+
+    [Header("Easy Mode?")]
+    public bool easyMode;
 
     private Rigidbody2D playerRigid;
 
@@ -21,8 +26,36 @@ public class CharacterController : MonoBehaviour
     }
     void Update()
     {
-        float delayMultiplier = baseDelayMultiplier * Vector2.Distance(player.transform.position, Input.mousePosition);
         LookAtMouse();
+        MovePlayer();
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+    private void LookAtMouse()
+    {
+        var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+
+
+        transform.rotation = Quaternion.AngleAxis((Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg), Vector3.forward);
+    }
+
+    private void MovePlayer()
+    {
+        
+        if (easyMode)    //Cap speed at some topSpeed on easy mode
+        {
+            if(speed >= topSpeed)
+            {
+                speed = topSpeed;
+            }
+        }
+        speed += Time.deltaTime * speedUpMultiplier;
+          //Increase player speed over time
+        float delayMultiplier = baseDelayMultiplier * Vector2.Distance(player.transform.position, Input.mousePosition);
+
         float reverseDistance = baseDelayMultiplier - Vector2.Distance(Input.mousePosition, playerRigid.position);
         float fixedDistance = Mathf.Clamp(reverseDistance, 0f, 4f);
         float delay = baseDelayMultiplier + fixedDistance;
@@ -33,19 +66,5 @@ public class CharacterController : MonoBehaviour
         Vector2 currentDirection = Vector2.Lerp(playerRigid.velocity, wantedDirection, Time.deltaTime * delay);
 
         playerRigid.velocity = currentDirection;
-
-    }
-    
-    void FixedUpdate()
-    {
-        //MovePlayer();
-    }
-
-    private void LookAtMouse()
-    {
-        var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-
-        
-        transform.rotation = Quaternion.AngleAxis((Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg), Vector3.forward);
     }
 }
